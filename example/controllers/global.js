3c2627c1bc1c50bce222e50f9ca209cbd0133166
petersirka <petersirka@gmail.com>
2013-01-05 13:46:33 +0100

    upravený príklad o statové kódy

--- example/controllers/global.js
+++ example/controllers/global.js
@@ -9,24 +9,28 @@ exports.init = function() {
 // Forbidden
 function error403() {
 	this.repository.title = 'Forbidden (403)';
+	this.statusCode = 403;
 	this.view('403');
 }
 
 // Not Found
 function error404() {
 	this.repository.title = 'Not Found (404)';
+	this.statusCode = 404;
 	this.view('404');
 }
 
 // Request Header Fields Too Large
 function error431() {
 	this.repository.title = 'Request Header Fields Too Large (431)';
+	this.statusCode = 431;
 	this.view('431');
 }
 
 // Internal Server Error
 function error500() {
 	this.repository.title = 'Internal Server Error (500)';
+	this.statusCode = 500;
 	this.view('500');
 }
 
