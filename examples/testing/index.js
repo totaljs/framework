d74a76dee16e169b97315ae9bfec45a78e07a165
petersirka <petersirka@gmail.com>
2013-01-30 19:17:25 +0100

    pridané komentáre do príkladu o testoch

--- examples/testing/index.js
+++ examples/testing/index.js
@@ -8,6 +8,9 @@ var debug = true;
 framework.init(http, debug, port);
 framework.controller('global');
 
+/*
+	ADD TO TEST
+*/
 
 framework.assert('Test URL 1', '/1/', function response (error, data, name, code, headers) {
 	assert.ok(code === 200 && data === '1', name);
@@ -25,6 +28,13 @@ framework.assert('Test URL 3', '/3/', function response (error, data, name, code
 	console.log('3');
 });
 
+
+/*
+	RUN TEST
+
+	@stop {Boolean} :: stop server?
+	@callback {Function}
+*/
 framework.test(true);
 
 console.log("http://127.0.0.1:{0}/".format(port));
