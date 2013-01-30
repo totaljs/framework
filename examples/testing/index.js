8561ab6c8ddbe31b24568eb7fb319123a0e0caa6
petersirka <petersirka@gmail.com>
2013-01-30 20:31:12 +0100

    upravený príklad testovania

--- examples/testing/index.js
+++ examples/testing/index.js
@@ -12,6 +12,15 @@ framework.controller('global');
 	ADD TO TEST
 */
 
+// @name {String} :: name of test
+// @url {String}
+// @callback {Function} :: function params @error {Error}, @data {String}, @name {String}, @statusCode {Number}, @headers {Object}
+// @method {String} :: optional default GET
+// @data {String} :: optional default empty string
+// @headers {Object} :: optional custom headers
+
+// framework.assert(name, url, callback, [method], [data], [headers])
+
 framework.assert('Test URL 1', '/1/', function response (error, data, name, code, headers) {
 	assert.ok(code === 200 && data === '1', name);
 	console.log('1');
