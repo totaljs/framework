b32b862d12a371f62dde48283b4c6381588a702f
Peter Širka <petersirka@gmail.com>
2013-01-31 11:38:25 +0100

    upravené http-cache + upravené príklady

--- examples/cache-http/controllers/global.js
+++ examples/cache-http/controllers/global.js
@@ -1,13 +1,45 @@
 var utils = require('partial.js/utils');
 
 exports.init = function() {
+	this.route('/dynamic/date/', viewDynamicModified);
+	this.route('/dynamic/etag/', viewDynamicEtag);
 	this.route('/date/', viewModified);
 	this.route('/etag/', viewEtag);
 };
 
+function viewDynamicModified() {
+	var self = this;
+
+	// Check header
+	//
+	// @date {Date} :: optional, default: new Date()
+	// @strict {Boolean} :: optional, default: false	
+	// 
+	// if @strict === false then header['if-modified-since'] > @date return 304
+	// if @strict === true then header['if-modified-since'] === @date return 304
+	//
+	// ifNotModified([date], [strict])
+
+	if (self.ifNotModified())
+		return;
+
+	self.layout('');
+	self.view('modified');
+}
+
 function viewModified() {
 
 	var self = this;
+
+	// Check header
+	//
+	// @date {Date} :: optional, default: new Date()
+	// @strict {Boolean} :: optional, default: false	
+	// 
+	// if @strict === false then header['if-modified-since'] > @date return 304
+	// if @strict === true then header['if-modified-since'] === @date return 304
+	//
+	// ifNotModified([date], [strict])
 
 	if (self.ifNotModified())
 		return;
@@ -21,6 +53,17 @@ function viewModified() {
 
 	// show Firebug and call 3x refresh
 	self.plain('modified');
+}
+
+
+function viewDynamicEtag() {
+	var self = this;
+
+	if (self.ifNotModified('123456'))
+		return;
+
+	self.layout('');
+	self.view('etag');
 }
 
 function viewEtag() {
