af4acacb9eeda276f586abd2811c9eeb6a6f2850
petersirka <petersirka@gmail.com>
2013-02-16 14:19:07 +0100

    updated templating: added formating (number, date and string) + updated example of templating

--- examples/templating/index.js
+++ examples/templating/index.js
@@ -38,16 +38,16 @@ var itemsInformation = {
 };
 
 var items = [
-	{ name: 'Product 01', price: 30.32 },
-	{ name: 'Product 02', price: 31.32 },
-	{ name: 'Product 03', price: 32.32 },
-	{ name: 'Product 04', price: 33.32 },
-	{ name: 'Product 05', price: 34.32 },
-	{ name: 'Product 06', price: 35.32 },
-	{ name: 'Product 07', price: 36.32 },
-	{ name: 'Product 08', price: 37.32 },
-	{ name: 'Product 09', price: 38.32 },
-	{ name: 'Product 10', price: 39.32 }
+	{ name: 'Product 01', price: 30.32, date: new Date().add('month', -1), example: { name: '1' }, fn: function() { return 'function 1'; }},
+	{ name: 'Product 02', price: 31.32, date: new Date().add('month', -2), example: { name: '2' }, fn: function() { return 'function 2'; }},
+	{ name: 'Product 03', price: 32.32, date: new Date().add('month', -3), example: { name: '3' }, fn: function() { return 'function 3'; }},
+	{ name: 'Product 04', price: 33.32, date: new Date().add('month', -4), example: { name: '4' }, fn: function() { return 'function 4'; }},
+	{ name: 'Product 05', price: 34.32, date: new Date().add('month', -5), example: { name: '5' }, fn: function() { return 'function 5'; }},
+	{ name: 'Product 06', price: 35.32, date: new Date().add('month', -6), example: { name: '6' }, fn: function() { return 'function 6'; }},
+	{ name: 'Product 07', price: 36.32, date: new Date().add('month', -7), example: { name: '7' }, fn: function() { return 'function 7'; }},
+	{ name: 'Product 08', price: 37.32, date: new Date().add('month', -7), example: { name: '8' }, fn: function() { return 'function 8'; }},
+	{ name: 'Product 09', price: 38.32, date: new Date().add('month', -8), example: { name: '9' }, fn: function() { return 'function 9'; }},
+	{ name: 'Product 10', price: 39.32, date: new Date().add('month', -9), example: { name: '0' }, fn: function() { return 'function 0'; }}
 ];
 
 global.database.itemsPaging = itemsPaging;
