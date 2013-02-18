c92819c7d16336626854bd2bd2714dc58ece88d4
Peter Širka <petersirka@gmail.com>
2013-02-18 11:16:35 +0100

    added new feature to view engine and added new example "forms" + fix problem with example "views"

---
+++ examples/forms/controllers/global.js
@@ -0,0 +1,49 @@
+var utils = require('partial.js/utils');
+
+exports.init = function() {
+	this.route('/', viewRegistration);
+	this.route('/', formRegistration, ['post']);
+};
+
+function viewRegistration() {
+	var self = this;
+
+	self.repository.title = 'Registration';	
+
+	var model = {
+		type: 0,
+		name: '',
+		email: '@',
+		password: '',
+		phone: '+421',
+		country: 'SK'
+	};
+
+	self.repository.country = ['', 'SK', 'CZ', 'EN', 'DE', 'AU', 'HU', 'PL', 'FR'];
+	self.repository.type = [
+		{ id: 0, name: '' },
+		{ id: 1, name: 'Developer' },
+		{ id: 2, name: 'Webdesigner' },
+		{ id: 3, name: 'Copywriter' },
+		{ id: 4, name: 'Consultant' }
+	];
+
+	self.view('registration', model);
+}
+
+// SEND FORM VIA XHR, THIS IS BAD EXAMPLE
+function formRegistration() {
+	var self = this;
+
+	self.repository.country = ['', 'SK', 'CZ', 'EN', 'DE', 'AU', 'HU', 'PL', 'FR'];
+	self.repository.type = [
+		{ id: 0, name: '' },
+		{ id: 1, name: 'Developer' },
+		{ id: 2, name: 'Webdesigner' },
+		{ id: 3, name: 'Copywriter' },
+		{ id: 4, name: 'Consultant' }
+	];
+
+	self.repository.isSuccess = true;
+	self.view('registration', self.post);
+}
