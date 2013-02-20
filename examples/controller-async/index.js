7a796cff08f8b51fac3f23776665b99299c1d5fb
Peter Širka <petersirka@gmail.com>
2013-02-20 10:49:11 +0100

    new example

---
+++ examples/controller-async/index.js
@@ -0,0 +1,12 @@
+var framework = require('partial.js');
+var http = require('http');
+
+var port = 8004;
+var debug = true;
+
+framework.init(http, debug, port);
+
+// Initialize controllers
+framework.controller('global');
+
+console.log("http://127.0.0.1:{0}/".format(port));
