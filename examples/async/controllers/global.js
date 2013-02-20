25c079b1eee8ab2e65351654eb2e9ef62d2a31df
Peter Širka <petersirka@gmail.com>
2013-02-20 10:53:24 +0100

    updated examples

--- examples/async/controllers/global.js
+++ examples/async/controllers/global.js
@@ -1,27 +1,44 @@
 var utils = require('partial.js/utils');
 
 exports.init = function() {
-	this.route('/', waitingArray);
-	this.route('/timeout/', waitingTimeout);
-}
+	this.route('/', viewHomepage);
+};
 
-function waitingArray() {
+function viewHomepage() {
 	var self = this;
-
-	var arr = [1, 2, 3, 4, 5];
-
-	arr.forEachAsync(function(o) {
-		
-		console.log(o);
-
-	}, function() {
-		self.plain('OK');
+	var builder = [];
+	
+	self.wait(function() {
+		utils.request('https://www.google.com', 'GET', null, function(err, data) {
+			var output = err ? 'error' : data.length.toString();
+			builder.push('www.google.com -> ' + output);
+			self.next();
+		});
 	});
-}
 
-function waitingTimeout() {
-	var self = this;
-	setTimeout(function() {
-		self.plain('OK');
-	}, 5000);
+	self.wait(function() {
+		utils.request('https://www.github.com', 'GET', null, function(err, data) {
+			var output = err ? 'error' : data.length.toString();
+			builder.push('www.github.com -> ' + output);
+			self.next();
+		});
+	});
+
+	self.wait(function() {
+		utils.request('http://www.yahoo.com', 'GET', null, function(err, data) {
+			var output = err ? 'error' : data.length.toString();
+			builder.push('www.yahoo.com -> ' + output);
+			self.next();
+		});
+	});
+
+	self.wait(function() {
+		utils.request('https://www.facebook.com', 'GET', null, function(err, data) {
+			var output = err ? 'error' : data.length.toString();
+			builder.push('www.facebook.com -> ' + output);
+			self.next();
+		});
+	});
+
+	self.viewAsync('homepage', builder);
 }
