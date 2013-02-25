9fc62b52f38451d4c243801176499d28ee3beb62
petersirka <petersirka@gmail.com>
2013-02-25 18:54:39 +0100

    updated binary and empty project

--- empty-project/index.js
+++ empty-project/index.js
@@ -5,10 +5,6 @@ var port = parseInt(process.argv[2] || '8000');
 var debug = true;
 
 framework.init(http, debug, port);
-
-// Initialize controllers
-framework.controller('global');
-
 //framework.test(true);
 
 console.log("http://127.0.0.1:{0}/".format(port));
