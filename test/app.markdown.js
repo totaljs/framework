295b03ed26b325bc85aa9d081464b3f9b040d918
petersirka <petersirka@gmail.com>
2013-03-01 23:49:18 +0100

    updated test

--- test/app.markdown.js
+++ test/app.markdown.js
@@ -11,6 +11,9 @@ markdown.onLines = function(type, value) {
 
 markdown.onUL = function(value) {
 
+	if (this.name === 'TEST')
+		return this.name;
+
 	var buffer = [];
 	value.forEach(function(o) {
 		buffer.push('<li>' + o.value + '</li>');
@@ -40,13 +43,14 @@ markdown.onImage = function(alt, url, width, height) {
 	return '<img src="' + url + '" alt="' + alt + '" widht="' + width + '" height="' + height + '" />';
 };
 
-assert.ok(markdown.load('> 1\n> 2') == '<p>12</p>', 'paragraph parser error');
-assert.ok(markdown.load('| 1\n| 2') == '<p>12</p>', 'paragraph parser error');
-assert.ok(markdown.load('- 1\n- 2') == '<ul><li>1</li><li>2</li></ul>', 'ul parser error');
-assert.ok(markdown.load('kontrola obrazka ![Test](http://google.sk/logo.png#300x200) a neviem ...') == 'kontrola obrazka <img src="http://google.sk/logo.png" alt="Te" widht="300" height="200" /> a neviem ...', 'img parser error');
-assert.ok(markdown.load('<www.google.sk> a [Google.sk]: http://google.sk.') == '<a href="www.google.sk">www.google.sk</a> a <a href="http://google.sk">Google.sk</a>.', 'ul parser error');
-assert.ok(markdown.load('*kurziva* __bold__') == '<i>kurziva</i> <b>bold</b>', 'format parser error');
-assert.ok(markdown.load('test test [Test] a {Test}') == 'test test <span>Test</span> a <span>Test</span>', 'keyword parser error');
+assert.ok(markdown.load('> 1\n> 2') === '<p>12</p>', 'paragraph parser error');
+assert.ok(markdown.load('| 1\n| 2') === '<p>12</p>', 'paragraph parser error');
+assert.ok(markdown.load('- 1\n- 2') === '<ul><li>1</li><li>2</li></ul>', 'ul parser error');
+assert.ok(markdown.load('kontrola obrazka ![Test](http://google.sk/logo.png#300x200) a neviem ...') === 'kontrola obrazka <img src="http://google.sk/logo.png" alt="Te" widht="300" height="200" /> a neviem ...', 'img parser error');
+assert.ok(markdown.load('<www.google.sk> a [Google.sk]: http://google.sk.') === '<a href="www.google.sk">www.google.sk</a> a <a href="http://google.sk">Google.sk</a>.', 'ul parser error');
+assert.ok(markdown.load('*kurziva* __bold__') === '<i>kurziva</i> <b>bold</b>', 'format parser error');
+assert.ok(markdown.load('test test [Test] a {Test}') === 'test test <span>Test</span> a <span>Test</span>', 'keyword parser error');
+assert.ok(markdown.load('- 1\n- 2', 'TEST') === 'TEST', 'name parameter');
 
 console.log('================================================');
 console.log('success - OK');
