e2481ef8ac13dc91535d9b2a5e0db19e4fe30389
petersirka <petersirka@gmail.com>
2013-03-13 20:00:19 +0100

    clean code

--- examples/views/index.js
+++ examples/views/index.js
@@ -2,7 +2,7 @@ var framework = require('partial.js');
 var http = require('http');
 
 var port = 8004;
-var debug = false;
+var debug = true;
 
 framework.init(http, debug, port);
 console.log("http://127.0.0.1:{0}/".format(port));
