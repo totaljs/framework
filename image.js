82df450cdcebc9c6e2944aaf339d556b4ef96c5d
Peter Širka <petersirka@gmail.com>
2013-03-26 08:54:49 +0100

    rename picture to image

--- image.js
+++ image.js
@@ -19,4 +19,4 @@
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
 // USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-module.exports = require("./lib/picture");
+module.exports = require("./lib/image");
