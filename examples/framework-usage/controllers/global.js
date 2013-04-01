7ad9e4f0efdccdbd56191730b6c7d9de8b9b931d
petersirka <petersirka@gmail.com>
2013-04-01 13:28:04 +0200

    updated framework usage

--- examples/framework-usage/controllers/global.js
+++ examples/framework-usage/controllers/global.js
@@ -2,9 +2,13 @@ exports.install = function(framework) {
 	framework.route('/', viewHomepage);
 };
 
+exports.usage = function() {
+	return 'controller usage';
+};
+
 function viewHomepage() {
 	var self = this;
 
 	// self.app.usage([detailed:bool default false])
-	self.plain(self.app.usage(true));
+	self.plain(self.framework.usage(true));
 }
