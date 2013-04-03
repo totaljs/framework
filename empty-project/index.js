96297a4e2462d87537521db681fac0508a90ac4a
Peter Širka <petersirka@gmail.com>
2013-04-03 08:56:54 +0200

    updated empty project

--- empty-project/index.js
+++ empty-project/index.js
@@ -4,6 +4,29 @@ var http = require('http');
 var port = parseInt(process.argv[2] || '8000');
 var debug = true;
 
+var index = process.argv.indexOf('backup');
+if (index !== -1) {
+	framework.backup(function(err, path) {
+		if (err)
+			console.log(err.toString());
+		else
+			console.log('Backup: ' + path);
+	});
+	return;
+}
+
+index = process.argv.indexOf('restore');
+if (index !== -1) {
+	var restore = process.argv[index + 1] || '';
+	framework.restore(restore, function(err, path) {
+		if (err)
+			console.log(err.toString());
+		else
+			console.log('Restore: ' + path);
+	});
+	return;
+}
+
 framework.run(http, debug, port);
 //framework.test(true);
 //framework.backup();
