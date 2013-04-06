ac5ad6bd6b510b474fc5288471bd992add34d592
petersirka <petersirka@gmail.com>
2013-04-06 22:36:18 +0200

    updated markdown

--- test/app.markdown.js
+++ test/app.markdown.js
@@ -52,6 +52,7 @@ assert.ok(markdown.load('\\\\ 1\n\\\\ 2') === '<p>12</p>', 'paragraph \\\\ parse
 assert.ok(markdown.load('- 1\n- 2') === '<ul><li>1</li><li>2</li></ul>', 'ul parser error');
 assert.ok(markdown.load('kontrola obrazka ![Test](http://google.sk/logo.png#300x200) a neviem ...') === 'kontrola obrazka <img src="http://google.sk/logo.png" alt="Test" width="300" height="200" /> a neviem ...', 'img parser error');
 assert.ok(markdown.load('kontrola obrazka ![Test](http://google.sk/logo.png#300x200)(www.google.sk) a neviem ...') === 'kontrola obrazka <a href="www.google.sk"><img src="http://google.sk/logo.png" alt="Test" width="300" height="200" /></a> a neviem ...', 'img url parser error');
+assert.ok(markdown.load('kontrola obrazka [![Test](http://google.sk/logo.png#300x200)](www.google.sk) a neviem ...') === 'kontrola obrazka <a href="www.google.sk"><img src="http://google.sk/logo.png" alt="Test" width="300" height="200" /></a> a neviem ...', 'img url parser error');
 assert.ok(markdown.load('<www.google.sk> a [Google.sk]: http://google.sk. [partial.js](www.partialjs.com)') === '<a href="www.google.sk">www.google.sk</a> a <a href="http://google.sk">Google.sk</a>. <a href="www.partialjs.com">partial.js</a>', 'link parser');
 assert.ok(markdown.load('*kurziva* __bold__') === '<i>kurziva</i> <b>bold</b>', 'format parser error');
 assert.ok(markdown.load('test test [Test] a {Test}') === 'test test <span>Test</span> a <span>Test</span>', 'keyword parser error');
