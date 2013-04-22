118bdee53a8a2e92228fce70825970591492b38f
Peter Širka <petersirka@gmail.com>
2013-04-22 14:26:15 +0200

    new example: responseStream

---
+++ examples/download-stream/controllers/default.js
@@ -0,0 +1,13 @@
+var utils = require('partial.js/utils');
+var fs = require('fs');
+
+exports.install = function(framework) {
+	framework.route('/', download);
+};
+
+function download() {
+	var self = this;
+	// documentation: http://www.partialjs.com/documentation/controller/
+	// @contentType {String}, @stream {Stream}, [@downloadName] {String}, [@headers] {Options}
+	self.stream('application/pdf', fs.createReadStream(self.pathPublic('company-profile.pdf')), 'about-us.pdf');
+}
