60e9d1638b61248305e897189c4d238f82051c30
petersirka <petersirka@gmail.com>
2013-04-26 22:09:32 +0200

    fixed EADDRINUSE

--- test/app.framework.js
+++ test/app.framework.js
@@ -7,7 +7,7 @@ var url = 'http://127.0.0.1:8001/';
 var errorStatus = 0;
 var max = 1000;
 
-framework.init(http, false, 8001);
+framework.init(http, false, 8000);
 
 var url = 'http://127.0.0.1:8001/';
 
