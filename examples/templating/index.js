56ada1c48ab3135ef111cfd06060e6ef6d61d8bb
petersirka <petersirka@gmail.com>
2013-05-11 11:19:37 +0200

    added new features: Condition & Current array index

--- examples/templating/index.js
+++ examples/templating/index.js
@@ -13,10 +13,10 @@ console.log("http://127.0.0.1:{0}/".format(port));
 framework.global.database = {};
 
 var category = [
-	{ url: '/', name: 'Shoes' },
-	{ url: '/', name: 'Shirts' },
-	{ url: '/', name: 'Jeans' },
-	{ url: '/', name: 'Accessories' }
+	{ url: '/', name: 'Shoes', alternate: false },
+	{ url: '/', name: 'Shirts', alternate: true },
+	{ url: '/', name: 'Jeans', alternate: false },
+	{ url: '/', name: 'Accessories', alternate: true }
 ];
 
 framework.global.database.category = category;
@@ -35,16 +35,16 @@ var itemsInformation = {
 };
 
 var items = [
-	{ name: 'Product 01', price: 30.32, date: new Date().add('month', -1), example: { name: '1' }, fn: function() { return 'function 1'; }},
-	{ name: 'Product 02', price: 31.32, date: new Date().add('month', -2), example: { name: '2' }, fn: function() { return 'function 2'; }},
-	{ name: 'Product 03', price: 32.32, date: new Date().add('month', -3), example: { name: '3' }, fn: function() { return 'function 3'; }},
-	{ name: 'Product 04', price: 33.32, date: new Date().add('month', -4), example: { name: '4' }, fn: function() { return 'function 4'; }},
-	{ name: 'Product 05', price: 34.32, date: new Date().add('month', -5), example: { name: '5' }, fn: function() { return 'function 5'; }},
-	{ name: 'Product 06', price: 35.32, date: new Date().add('month', -6), example: { name: '6' }, fn: function() { return 'function 6'; }},
-	{ name: 'Product 07', price: 36.32, date: new Date().add('month', -7), example: { name: '7' }, fn: function() { return 'function 7'; }},
-	{ name: 'Product 08', price: 37.32, date: new Date().add('month', -7), example: { name: '8' }, fn: function() { return 'function 8'; }},
-	{ name: 'Product 09', price: 38.32, date: new Date().add('month', -8), example: { name: '9' }, fn: function() { return 'function 9'; }},
-	{ name: 'Product 10', price: 39.32, date: new Date().add('month', -9), example: { name: '0' }, fn: function() { return 'function 0'; }}
+	{ name: 'Product 01', price: 30.32, date: new Date().add('month', -1), example: { name: '1' }, fn: function() { return 'function 1'; } },
+	{ name: 'Product 02', price: 31.32, date: new Date().add('month', -2), example: { name: '2' }, fn: function() { return 'function 2'; } },
+	{ name: 'Product 03', price: 32.32, date: new Date().add('month', -3), example: { name: '3' }, fn: function() { return 'function 3'; } },
+	{ name: 'Product 04', price: 33.32, date: new Date().add('month', -4), example: { name: '4' }, fn: function() { return 'function 4'; } },
+	{ name: 'Product 05', price: 34.32, date: new Date().add('month', -5), example: { name: '5' }, fn: function() { return 'function 5'; } },
+	{ name: 'Product 06', price: 35.32, date: new Date().add('month', -6), example: { name: '6' }, fn: function() { return 'function 6'; } },
+	{ name: 'Product 07', price: 36.32, date: new Date().add('month', -7), example: { name: '7' }, fn: function() { return 'function 7'; } },
+	{ name: 'Product 08', price: 37.32, date: new Date().add('month', -7), example: { name: '8' }, fn: function() { return 'function 8'; } },
+	{ name: 'Product 09', price: 38.32, date: new Date().add('month', -8), example: { name: '9' }, fn: function() { return 'function 9'; } },
+	{ name: 'Product 10', price: 39.32, date: new Date().add('month', -9), example: { name: '0' }, fn: function() { return 'function 0'; } }
 ];
 
 framework.global.database.itemsPaging = itemsPaging;
