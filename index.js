2c894f2e72f7e317eafd3f6ba189ea75c77336ff
petersirka <petersirka@gmail.com>
2013-05-25 15:02:56 +0200

    added CSS auto prefixer

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ process.chdir(directory);
 require('./prototypes');
 
 function Framework() {
-	this.version = 1238;
+	this.version = 1239;
 	this.versionNode = parseInt(process.version.replace('v', '').replace(/\./g, ''), 10);
 
 	this.handlers = {
