a0b6734b4c103c8b9da18dd4c4d87fde45527e02
petersirka <petersirka@gmail.com>
2013-05-27 20:58:46 +0200

    updated version

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ process.chdir(directory);
 require('./prototypes');
 
 function Framework() {
-	this.version = 1239;
+	this.version = 1240;
 	this.versionNode = parseInt(process.version.replace('v', '').replace(/\./g, ''), 10);
 
 	this.handlers = {
