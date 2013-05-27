56a3f48ae2b28a854c880b6568b10e39ce310f77
petersirka <petersirka@gmail.com>
2013-05-27 20:55:24 +0200

    added controller.head()

--- examples/views-head/controllers/global.js
+++ examples/views-head/controllers/global.js
@@ -2,33 +2,13 @@ var utils = require('partial.js/utils');
 
 exports.install = function(framework) {
 	framework.route('/', viewHomepage);
-	framework.route('/controller/', viewHomepageMetaController);
 };
 
 function viewHomepage() {
 	var self = this;
+
+	// append <script type="text/javascript" src="test.js"></script> to head
+	self.head('test.js');
+
 	self.view('homepage');
-}
-
-function viewHomepageMetaController() {
-	var self = this;
-	
-	self.meta('title controller', 'description controller', 'keywords controller');
-	
-	// self.meta('title controller', 'description controller', 'meta image_src');
-	//
-	// you can write own meta render
-	//
-	// framework.onMeta = function() {
-	//    arguments.forEach(function(o) {
-	//       ...
-	//	  });
-	//    return '<title>HA HA HA</title>';
-	// };
-	//
-	// ===========================================================================
-	// look at example: framework-custom
-	// ===========================================================================
-
-	self.view('homepage-controller');
 }
