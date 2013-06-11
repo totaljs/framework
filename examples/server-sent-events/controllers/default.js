f3616f56d830aea3a772d8eda8c6e080e3ec8d8c
petersirka <petersirka@gmail.com>
2013-06-11 18:44:44 +0200

    added new example

---
+++ examples/server-sent-events/controllers/default.js
@@ -0,0 +1,45 @@
+var utils = require('partial.js/utils');
+
+exports.install = function(framework) {
+	framework.route('/', viewHomepage);
+	framework.route('/one-way-messaging/', viewSSE, ['sse']);
+};
+
+function viewHomepage() {
+	var self = this;
+	self.view('homepage');
+}
+
+function viewSSE() {
+
+	var self = this;
+	var indexer = 0;
+	var counter = parseInt(self.lastEventID || '0');
+
+	console.log('Last event ID:', self.lastEventID);
+
+	var interval = setInterval(function() {
+
+		/*
+			Send data to client - One way messaging
+
+			@data {String or Object}
+			@eventname {String} :: default null
+			@id {String} :: default null (Last Event ID)
+			@retry {Number} :: in milliseconds, default config['default-request-timeout']
+
+			controller.sse(data, [eventname], [id], [retry])
+		*/
+		self.sse({ counter: counter++, message: utils.GUID(10) }, null, counter);
+
+		// reconnect by the @retry
+		indexer++;
+		if (indexer > 10) {
+			clearInterval(interval);
+			self.close();
+		}
+
+	}, 1000);
+
+
+}
