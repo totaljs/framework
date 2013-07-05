c20be1658474828abf5d29389f327830d942b289
petersirka <petersirka@gmail.com>
2013-07-05 23:14:17 +0200

    updated mail sender code

--- examples/email-templating/index.js
+++ examples/email-templating/index.js
@@ -28,14 +28,30 @@ framework.mail = function(email, name, params) {
 	var body = self.resource('en', 'mail-' + name, '');
 	var template = self.resource('en', 'mail', '').replace('{body}', body.params(params));
 
-	// mail.send(smtp, mailFrom, mailTo, mailCC, subject, body, [senderName], [mailReply], [userName], [userPassword]);
-	// mail.debug = true;
-	mail.send(self.config['mail-smtp'], self.config['mail-from'], email, null, subject, template, self.config['mail-from-name']);
+	// OLD VERSION partial.js
+	// mail.send(self.config['mail-smtp'], self.config['mail-from'], email, null, subject, template, self.config['mail-from-name']);
+
+	// partial.js +v1.2.4-4
+	var message = new mail.Message(subject, template);
+
+	// message.subject;
+	// message.body;
+
+	message.sender(self.config['mail-from'], self.config['mail-from-name']);
+	message.to(email);
+
+	// message.cc('@');
+	// message.bcc('@');
+	// message.reply('@');
+	// message.attachmet('/users/desktop/petersirka/filename.txt', 'name.txt');
+	// message.send(smtp, options);
+
+	message.send(self.config['mail-smtp'], { user: '', password: ''});
 	return true;
 };
 
-mail.on('error', function(error) {
-	console.log('ERROR --->', error);
+mail.on('error', function(error, description) {
+	console.log('ERROR --->', error, description);
 });
 
 console.log("http://127.0.0.1:{0}/".format(port));
