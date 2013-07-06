f49480d6d95c6277ecbfc451ba904443448c1fd6
petersirka <petersirka@gmail.com>
2013-07-06 09:22:00 +0200

    new example

---
+++ examples/authorization-www-basic/controllers/global.js
@@ -0,0 +1,20 @@
+exports.install = function(framework) {
+	framework.route('/', authorization);
+};
+
+
+function authorization() {
+
+	var self = this;
+	var user = self.baa('This is secure area');
+
+	if (user === null)
+		return;
+
+	if (user.name !== 'peter' || user.password !== '123456') {
+		self.view401();
+		return;
+	}
+
+	self.plain('You are authorized.');
+}
