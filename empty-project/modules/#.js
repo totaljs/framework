740a3a44eaf3f3d8e38f4d25233441fac41bf951
petersirka <petersirka@gmail.com>
2013-07-12 22:33:54 +0200

    renamed onLoaded to onLoad

--- empty-project/modules/#.js
+++ empty-project/modules/#.js
@@ -1,7 +1,7 @@
 // your custom framework prototypes - https://github.com/petersirka/partial.js/blob/master/empty-project/modules/%23.js
 // this code it run automatically
 
-// exports.onLoaded = function(self) {};
+// exports.onLoad = function(self) {};
 // exports.onError = function(err, name, uri) {};
 // exports.onAuthorization = function(req, res, callback) { callback(false); };
 // exports.onPrefix = function(req) { return ''; };
