6b9f3b9d48d2fbdf29ed5ae7878e0840c81012ee
petersirka <petersirka@gmail.com>
2013-10-03 22:51:27 +0200

    fixed: ip

--- examples/websocket/index.js
+++ examples/websocket/index.js
@@ -4,5 +4,5 @@ var http = require('http');
 var port = 8004;
 var debug = true;
 
-framework.run(http, debug, port);
+framework.run(http, debug, port, '127.0.0.1');
 console.log("http://{0}:{1}/".format(framework.ip, framework.port));
