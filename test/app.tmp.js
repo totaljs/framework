36085bbed0cbdc8171783668161cf20093c8fc19
Peter Širka <petersirka@gmail.com>
2013-10-14 15:28:07 +0200

    new async testing

--- test/app.tmp.js
+++ test/app.tmp.js
@@ -8,107 +8,232 @@ var fs = require('fs');
 var events = require('events');
 var crypto = require('crypto');
 var framework = require('../index');
+var ENCODING = 'utf8';
+var UNDEFINED = 'undefined';
+var STRING = 'string';
+var FUNCTION = 'function';
+var NUMBER = 'number';
+var OBJECT = 'object';
+var BOOLEAN = 'boolean';
 
-var BOUNDARY = '----' + Math.random().toString(16).substring(2);
+function AsyncTask(owner, name, fn, cb, waiting) {
 
-function send(url) {
-
-	var uri = urlParser.parse(url);
-	var h = {};
-	var indexer = 0;
-
-	h['Content-Type'] = 'multipart/x-mixed-replace; boundary=' + BOUNDARY;
-	//h['Content-Type'] = 'multipart/form-data; boundary=' + BOUNDARY;
-
-	var options = { protocol: uri.protocol, auth: uri.auth, method: 'POST', hostname: uri.hostname, port: uri.port, path: uri.path, agent: false, headers: h };
-
-	var response = function(res) {
-		var buffer = '';
-
-		res.on('data', function(chunk) {
-			buffer += chunk.toString('utf8');
-		});
-
-		res.on('end', function() {
-			console.log(res.statusCode, buffer);
-		});
-
-		res.resume();
-		console.log('response');
+	this.handlers = {
+		oncomplete: this.complete.bind(this)
 	};
 
-	var con = http;
+	this.isRunning = 0;
+	this.owner = owner;
+	this.name = name;
+	this.fn = fn;
+	this.cb = cb;
+	this.waiting = waiting;
+}
 
-
-	var req = con.request(options, response);
-
-	req.on('error', function(error) {
-		console.log('ERROR', error, error.stack);
-	});
-
-	function sendfile(filename, cb) {
-		console.log(filename);
-		var header = '\r\n\r\n--' + BOUNDARY + '\r\nContent-Disposition: form-data; name="File"; filename="'+path.basename(filename)+'"\r\nContent-Type: image/jpeg\r\n\r\n';
-		req.write(header);
-		var stream = fs.createReadStream(filename);
-		stream.pipe(req, { end: false });
-		stream.on('end', cb);
+AsyncTask.prototype.run = function() {
+	var self = this;
+	try
+	{
+		self.isRunning = 1;
+		self.owner.tasksWaiting[self.name] = true;
+		self.owner.emit('begin', self.name);
+		self.fn(self.handlers.oncomplete);
+	} catch (ex) {
+		self.owner.emit('error', self.name, ex);
+		self.complete();
 	}
-
-	function run() {
-		indexer++;
-
-		if (indexer > 5) {
-			req.end('\r\n\r\n--' + BOUNDARY + '--');
-			console.log('END');
-			return;
-		}
-
-		setTimeout(function() {
-			sendfile('/users/petersirka/desktop/aaaaa/' + indexer + '.jpg', run);
-		}, 500);
-
-	}
-
-	run();
+	return self;
 };
 
-//send('http://127.0.0.1:8004/');
-//send('http://127.0.0.1:8001/live/incoming/');
+AsyncTask.prototype.complete = function() {
 
-function arrayFindAll(arr, filter) {
+	var item = this;
+	var self = item.owner;
 
-	var length = arr.length;
-	var selected = [];
+	item.isRunning = 2;
+
+	delete self.tasksPending[item.name];
+	delete self.tasksWaiting[item.name];
+
+	self.reload();
+	self.refresh();
+	self.emit('end', item.name);
+
+	if (item.cb)
+		item.cb();
+
+	return self;
+};
+
+function Async(owner) {
+
+	this._count = 0;
+	this._isRunning = false;
+	this._isNew = false;
+
+	this.owner = owner;
+	this.onComplete = [];
+
+	this.tasksPending = {};
+	this.tasksWaiting = {};
+	this.tasksAll = [];
+}
+
+Async.prototype = {
+	get count() {
+		return this._count;
+	}
+}
+
+Async.prototype.__proto__ = new events.EventEmitter();
+
+Async.prototype.reload = function() {
+	var self = this;
+	self.tasksAll = Object.keys(self.tasksPending);
+	return self;
+};
+
+Async.prototype.await = function(name, fn, cb) {
+
+	var self = this;
+
+	if (typeof(name) === FUNCTION) {
+		cb = fn;
+		fn = name;
+		name = utils.GUID(6);
+	}
+
+	if (typeof(self.tasksPending[name]) !== UNDEFINED)
+		return false;
+
+	self._count++;
+	self.tasksPending[name] = new AsyncTask(self, name, fn, cb, null);
+	self.reload();
+	self.refresh();
+
+	return true;
+};
+
+Async.prototype.wait = function(name, waitingFor, fn, cb) {
+
+	var self = this;
+
+	if (typeof(waitingFor) === FUNCTION) {
+		cb = fn;
+		fn = waitingFor;
+		waitingFor = name;
+		name = utils.GUID(6);
+	}
+
+	if (typeof(self.tasksPending[name]) !== UNDEFINED)
+		return false;
+
+	self._count++;
+	self.tasksPending[name] = new AsyncTask(self, name, fn, cb, waitingFor);
+	self.reload();
+	self.refresh();
+
+	return true;
+
+};
+
+Async.prototype.complete = function(fn) {
+	return this.run(fn);
+};
+
+Async.prototype.run = function(fn) {
+	var self = this;
+	self._isRunning = true;
+	self.onComplete.push(fn);
+	self.refresh();
+	return self;
+};
+
+Async.prototype.isRunning = function(name) {
+
+	var self = this;
+
+	if (!name)
+		return self._isRunning;
+
+	var task = self.tasksPending[name];
+	if (!task)
+		return false;
+
+	return task.isRunning === 1;
+};
+
+Async.prototype.isWaiting = function(name) {
+	var self = this;
+
+	var task = self.tasksPending[name];
+	if (!task)
+		return false;
+
+	return task.isRunning === 0;
+};
+
+Async.prototype.refresh = function(name) {
+
+	var self = this;
+
+	if (!self._isRunning)
+		return self;
+
+	var length = self.tasksAll.length;
 
 	for (var i = 0; i < length; i++) {
-		var value = arr[i];
-		if (filter(value))
-			selected.push(value);
+
+		var task = self.tasksPending[self.tasksAll[i]];
+
+		if (task.isRunning !== 0)
+			continue;
+
+		if (task.waiting !== null && typeof(self.tasksWaiting[task.waiting]) !== UNDEFINED)
+			continue;
+
+		task.run();
 	}
 
-	return selected;
-};
-
-function arrayFindAllWithIndex(arr, filter) {
-
-	var length = arr.length;
-	var selected = [];
-
-	for (var i = 0; i < length; i++) {
-		var value = arr[i];
-		// i === index
-		if (filter(value, i))
-			selected.push(value);
+	if (length === 0) {
+		self._isRunning = false;
+		self.emit('complete');
+		length = self.onComplete.length;
+		for (var i = 0; i < length; i++)
+			self.onComplete[i]();
+		self.onComplete = [];
 	}
 
-	return selected;
+	return self;
 };
 
-builders.validation('tbl_user', ['Name']);
-builders.schema('tbl_user', { Id: Number, Name: 'string(5)' }, 'Id', false);
+var async = new Async();
 
-console.log(builders.validation('tbl_user'));
+async.await('1', function(next) {
+	setTimeout(function() {
+		console.log('1');
+		next();
+	}, 500)
+});
+
+async.wait('2', '1', function(next) {
+	setTimeout(function() {
+		console.log('2');
+		next();
+	}, 500)
+});
+
+async.on('begin', function(name) {
+	console.log('BEGIN', name);
+});
+
+async.on('end', function(name) {
+	console.log('END', name, this.isRunning());
+});
+
+async.run(function() {
+	console.log('COMPLETED');
+});
 
 
 // message.attachment('/users/petersirka/desktop/wall.png');
