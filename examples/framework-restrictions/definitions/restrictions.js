40c90c48066f6baeda68c642b0e6d4babbf2d8c4
Peter Širka <petersirka@gmail.com>
2013-10-18 10:57:27 +0200

    added: new example

---
+++ examples/framework-restrictions/definitions/restrictions.js
@@ -0,0 +1,12 @@
+
+// Allow only this IP address
+// framework.restrictions.allow('192.168.11.95');
+
+// Disallow only this IP address
+framework.restrictions.disallow('192.168.11.95');
+
+// Allow only Chrome
+// framework.restrictions.allow('user-agent', /Chrome/g);
+
+// Disallow only Chrome
+framework.restrictions.disallow('user-agent', /Chrome/g);
