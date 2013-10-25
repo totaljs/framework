c1abf40706e8a6a696a48fda9371863fae7ea151
Peter Širka <petersirka@gmail.com>
2013-10-25 14:06:30 +0200

    updated: version

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ process.chdir(directory);
 process.maxTickDepth = 300;
 
 function Framework() {
-	this.version = 1271;
+	this.version = 1280;
 	this.versionNode = parseInt(process.version.replace('v', '').replace(/\./g, ''), 10);
 
 	this.handlers = {
