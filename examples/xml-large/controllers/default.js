b176c66bdcc13c56653137ddc973bb854b2ec005
petersirka <petersirka@gmail.com>
2013-11-18 20:52:01 +0100

    updated: example

--- examples/xml-large/controllers/default.js
+++ examples/xml-large/controllers/default.js
@@ -5,7 +5,9 @@ exports.install = function(framework) {
 function xml() {
 	var self = this;
 
-	self.res.writeHead(200, { 'Content-Type': 'text/xml' });	
+	self.custom();
+
+	self.res.writeHead(200, { 'Content-Type': 'text/xml' });
 	self.res.write('<xml>\n')
 
 	// write async loop
@@ -14,16 +16,13 @@ function xml() {
 	self.res.write('<item value="1" />\n'.indent(4));
 	self.res.write('<item value="1" />\n'.indent(4));
 	self.res.write('<item value="1" />\n'.indent(4));
-	// .... 
-	// .... 
-	// .... 
-	// .... 
-	// .... 
+	// ....
+	// ....
+	// ....
+	// ....
+	// ....
 	// }
 	self.res.write('</xml>');
-
-	// IMPORTANT!!!
-	// this prevent a timeout and update internal stats
-	self.close();
+	self.res.end();
 }
-	
+
