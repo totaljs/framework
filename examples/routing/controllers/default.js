512981235d252b90ec9aeb467e8da0729834f005
Peter Širka <petersirka@gmail.com>
2013-11-25 11:35:45 +0100

    compile current scripts + new examples

--- examples/routing/controllers/default.js
+++ examples/routing/controllers/default.js
@@ -1,4 +1,5 @@
 exports.init = function(framework) {
+	// Documentation: http://docs.partialjs.com/Framework/#framework.route
 	framework.route('/contact/', view_contact);
 	framework.route('/products/', view_products);
 	framework.route('/products/{category}/', view_products);
@@ -10,19 +11,45 @@ exports.init = function(framework) {
 	// this.routeFile(name, funcValidation, funcExecute);
 	// @name {String}
 	// @funcValidation {Function} :: params: {req}, {res}, return {Boolean};
-	// @funcExecute {Function} :: params: {req}, {res};	
+	// @funcExecute {Function} :: params: {req}, {res};
 
 	// route: all txt files
-	framework.file('my route file for .txt', function onValidation(req, res) {
-		
+	// Documentation: http://docs.partialjs.com/Framework/#framework.file
+	// Try: http://127.0.0.4/test.txt
+	framework.file('.txt', function(req, res) {
 		// valid request
 		return req.url.indexOf('.txt') !== -1;
+	}, static_txt);
 
-	}, function onExecute(req, res) {
-		
-		// generate response
-		// this === framework
-		this.responseContent(req, res, 200, 'Server time: ' + new Date().toString(), 'text/plain');
+	// route: all jpg files
+	// all images will resized about 50%
+	// Documentation: http://docs.partialjs.com/Framework/#framework.file
+	// Try: http://127.0.0.4/header.jpg
+	framework.file('.jpg', function(req, res) {
+		// valid request
+		return req.url.indexOf('.jpg') !== -1;
+	}, static_jpg);
+}
+
+function static_txt(req, res) {
+	// generate response
+	// this === framework
+	// Documentation: http://docs.partialjs.com/Framework/#framework.responsContent
+	this.responseContent(req, res, 200, 'Server time: ' + new Date().toString(), 'text/plain');
+}
+
+function static_jpg(req, res) {
+	// generate response
+	// this === framework
+	// Documentation: http://docs.partialjs.com/Framework/#framework.responseImage
+	this.responseImage(req, res, this.path.public(req.url), function (image) {
+
+		// image === FrameworkImage
+		// http://docs.partialjs.com/FrameworkImage/
+
+		image.resize('50%');
+		image.quality(80);
+		image.clean();
 
 	});
 }
---
+++ examples/workers/workers/weather.js
@@ -0,0 +1,10 @@
+var utils = require('partial.js/utils');
+
+utils.request('http://api.openweathermap.org/data/2.5/weather?q=London,uk', 'GET', '', function(err, data) {
+
+	if (!err)
+		process.send(JSON.parse(data));
+
+	process.exit();
+
+});
