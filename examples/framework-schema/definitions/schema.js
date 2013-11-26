3ec47235f5a898093cf21275745d0bdfcd13201f
petersirka <petersirka@gmail.com>
2013-11-26 22:09:01 +0100

    added: joining of models

--- examples/framework-schema/definitions/schema.js
+++ examples/framework-schema/definitions/schema.js
@@ -1,7 +1,8 @@
+// Create model schemas
 
-// Create data schemas
-
-builders.schema('order', { firstname: 'string(30)', lastname: 'string(40)', email: 'string(120)', telephone: 'string(20)', address: 'string', ip: 'string', created: Date, updated: Date }, function (name) {
+// [product] === array of schema('product')
+// product = schema('product')
+builders.schema('order', { products: '[product]', firstname: 'string(30)', lastname: 'string(40)', email: 'string(120)', telephone: 'string(20)', address: 'string', ip: 'string', created: Date, updated: Date }, function (name) {
 
 	// Default values
 	switch (name) {
@@ -14,6 +15,8 @@ builders.schema('order', { firstname: 'string(30)', lastname: 'string(40)', emai
 
 });
 
+builders.schema('product', { name: 'string(30)', price: 'number' });
+
 builders.schema('contactform', { name: 'string(30)', email: 'string(120)', message: 'string(8000)', ip: 'string', created: Date }, function(name) {
 	if (name === 'created')
 		return new Date();
@@ -21,5 +24,5 @@ builders.schema('contactform', { name: 'string(30)', email: 'string(120)', messa
 
 // Serve for controller.validation()
 
-builders.validation('order', ['firstname', 'lastname', 'email', 'telephone', 'address']);
+builders.validation('order', ['firstname', 'lastname', 'email', 'telephone', 'address', 'products']);
 builders.validation('contactform', ['name', 'email', 'message']);
--- examples/framework-schema/definitions/validation.js
+++ examples/framework-schema/definitions/validation.js
@@ -1,14 +1,17 @@
 
 
-framework.onValidation = function (name, value) {
-	
+framework.onValidation = function (name, value, path) {
+
 	switch (name) {
 		case 'email':
 			return (value || '').isEmail();
+		case 'price':
+			return value.parseFloat() > 0;
 		case 'firstname':
 		case 'lastname':
 		case 'telephone':
 		case 'address':
+		case 'name':
 			return (value || '').length > 0;
 	}
 
