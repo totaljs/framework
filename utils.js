2a091e29f9f1d1fe509a891b7b02c7eb42da41f6
Peter Širka <petersirka@gmail.com>
2013-11-26 15:03:13 +0100

    added: utils.distance()

--- utils.js
+++ utils.js
@@ -934,6 +934,15 @@ exports.decode_WS = function(data) {
     return output;
 };
 
+exports.distance = function(lat1, lon1, lat2, lon2) {    
+    var R = 6371;
+    var dLat = (lat2 - lat1).toRad();
+    var dLon = (lon2 - lon1).toRad();
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return (R * c).floor(3);
+};
+
 /*
 	@type {String}
 	@value {Number}
@@ -1828,6 +1837,12 @@ Number.prototype.discount = function(percentage, decimals) {
 Number.prototype.parseDate = function(plus) {
 	return new Date(this + (plus || 0));
 };
+
+if (typeof (Number.prototype.toRad) === "undefined") {
+    Number.prototype.toRad = function () {
+        return this * Math.PI / 180;
+    };
+}
 
 Boolean.prototype.condition = function(ifTrue, ifFalse) {
 	return (this ? ifTrue : ifFalse) || '';
