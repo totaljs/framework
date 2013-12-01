69f77b72dbb849d8d29c5d73c349820cec74f493
petersirka <petersirka@gmail.com>
2013-12-01 17:49:54 +0100

    fixed: controller.status

--- empty-project/controllers/default.js
+++ empty-project/controllers/default.js
@@ -1,5 +1,6 @@
 exports.install = function(framework) {
 	framework.route('/', view_homepage);
+	framework.route('#400', error400);
 	framework.route('#401', error401);
 	framework.route('#403', error403);
 	framework.route('#404', error404);
@@ -18,46 +19,53 @@ exports.functions = function() {
 };
 */
 
+// Bad Request
+function error400() {
+	var self = this;
+	self.status = 400;
+	self.plain(utils.httpStatus(self.status));
+}
+
 // Unauthorized
 function error401() {
 	var self = this;
-	self.statusCode = 401;
-	self.plain(utils.httpStatus(self.statusCode));
+	self.status = 401;
+	self.plain(utils.httpStatus(self.status));
 }
 
 // Forbidden
 function error403() {
 	var self = this;
-	self.statusCode = 403;
-	self.plain(utils.httpStatus(self.statusCode));
+	self.status = 403;
+	self.plain(utils.httpStatus(self.status));
 }
 
 // Not Found
 function error404() {
 	var self = this;
-	self.statusCode = 404;
-	self.plain(utils.httpStatus(self.statusCode));
+	self.status = 404;
+	self.plain(utils.httpStatus(self.status));
 }
 
 // Request Timeout
 function error408() {
 	var self = this;
-	self.statusCode = 408;
-	self.plain(utils.httpStatus(self.statusCode));
+	self.status = 408;
+	self.plain(utils.httpStatus(self.status));
 }
 
 // Request Header Fields Too Large
 function error431() {
 	var self = this;
-	self.statusCode = 431;
-	self.plain(utils.httpStatus(self.statusCode));
+	self.status = 431;
+	self.plain(utils.httpStatus(self.status));
 }
 
 // Internal Server Error
 function error500() {
 	var self = this;
-	self.statusCode = 500;
-	self.plain(utils.httpStatus(self.statusCode));
+	self.status = 500;
+	self.plain(utils.httpStatus(self.status));
 }
 
 function view_homepage() {
