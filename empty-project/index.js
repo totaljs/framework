5e4888298ad07005774c605b32dd4b50c71e6501
Peter Širka <petersirka@gmail.com>
2013-12-13 08:17:25 +0100

    fixed: index.js

--- empty-project/index.js
+++ empty-project/index.js
@@ -5,8 +5,3 @@ var port = parseInt(process.argv[2] || '8000', 10);
 var debug = true;
 
 framework.run(http, debug, port);
-
-// framework.test(true);
-
-console.log('partial.js v' + framework.version);
-console.log('http://{0}:{1}/'.format(framework.ip, framework.port));
