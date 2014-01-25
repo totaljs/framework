d455fb049aaf9b361c98e78add18ce739c56bf7b
Peter Širka <petersirka@gmail.com>
2014-01-25 16:37:39 +0100

    clean code

--- empty-project/tests/global.js
+++ empty-project/tests/global.js
@@ -14,8 +14,12 @@ exports.run = function(framework) {
 	// @headers {Object} :: optional custom headers
 
 	// framework.assert(name, url, callback, [method], [data], [headers])
-/*
-	framework.assert('Test URL 1', '/1/', function response (error, data, name, code, headers) {
+
+	framework.assert('Test URL 1', '/1/', function (error, data, name, code, headers) {
 		assert.ok(code === 200 && data === '1', name);
-	});*/
+	});
+
+	framework.assert('Test methods', function(name) {
+		assert.ok('1' !== '1', name);
+	});
 };
