e603b8a7f1e4f4a19b339a79777b10314cff9bfd
Peter Širka <petersirka@gmail.com>
2014-07-15 11:20:22 +0200

    updated: empty-project

---
+++ empty-project/definitions/modules.js
@@ -0,0 +1,9 @@
+/**
+ * External modules
+ */
+
+// Real-time web tracking
+INSTALL('module', 'https://modules.totaljs.com/webcounter/v1.00/webcounter.js', { url: '/webcounter/' });
+
+// A simple DDOS protection
+INSTALL('module', 'https://modules.totaljs.com/ddos/v1.00/ddos.js');
