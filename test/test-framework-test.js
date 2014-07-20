1b980e95de6db7c6e4ec3ee59b14a37fbe990347
Peter Širka <petersirka@gmail.com>
2014-07-20 22:08:00 +0200

    new change

--- test/test-framework-test.js
+++ test/test-framework-test.js
@@ -1,1 +1,10 @@
-require('../index').http('test', 8001);
+
+
+var Image = require('../image');
+
+
+var img = Image.load('/users/petersirka/desktop/b.jpg', false);
+
+img.resize('100%', '100%');
+img.watermark('/users/petersirka/desktop/a.jpg', 'center');
+img.save('/users/petersirka/desktop/output.jpg');
