1040d5c48bf48ce224f22dcc4bc05b9cf64c8ba2
Peter Širka <petersirka@gmail.com>
2014-08-05 08:51:30 +0200

    added: new vendor attributes into the CSS

--- internal.js
+++ internal.js
@@ -833,7 +833,7 @@ function compile_jscss(css) {
 function autoprefixer(value) {
 
     // 'box-shadow', 'border-radius'
-    var prefix = ['appearance', 'column-count', 'column-gap', 'column-rule', 'display', 'transform', 'transform-origin', 'transition', 'user-select', 'animation', 'animation-name', 'animation-duration', 'animation-timing-function', 'animation-delay', 'animation-iteration-count', 'animation-direction', 'animation-play-state', 'opacity', 'background', 'background-image', 'font-smoothing', 'text-size-adjust'];
+    var prefix = ['appearance', 'column-count', 'column-gap', 'column-rule', 'display', 'transform', 'transform-style', 'transform-origin', 'transition', 'user-select', 'animation', 'perspective', 'animation-name', 'animation-duration', 'animation-timing-function', 'animation-delay', 'animation-iteration-count', 'animation-direction', 'animation-play-state', 'opacity', 'background', 'background-image', 'font-smoothing', 'text-size-adjust', 'backface-visibility'];
 
     value = autoprefixer_keyframes(value);
 
