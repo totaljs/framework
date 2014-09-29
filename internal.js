5ca63647e54ffb0ebf8c75648ce5977dd56e42f2
Peter Širka <petersirka@gmail.com>
2014-09-29 20:11:15 +0200

    fixed: routing

--- internal.js
+++ internal.js
@@ -490,6 +490,9 @@ exports.routeCompareFlags2 = function(req, route, noLoggedUnlogged) {
     if (!HTTPVERBS[req.method])
         return 0;
 
+    if (route.flags.indexOf(req.method.toLowerCase()) === -1)
+        return 0;
+
     for (var i = 0, length = req.flags.length; i < length; i++) {
 
         var flag = req.flags[i];
