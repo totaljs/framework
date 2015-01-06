b6263d251fe8929747f83664aa37e8d0994390b4
Peter Širka <petersirka@gmail.com>
2015-01-06 23:09:47 +0100

    updated: version

--- test/test-utils.js
+++ test/test-utils.js
@@ -345,10 +345,10 @@ function others() {
 	assert.ok(utils.encode('<b>total.js</b>"&nbsp;') === '&lt;b&gt;total.js&lt;/b&gt;&quot;&amp;nbsp;', 'utils.encode()');
 	assert.ok(utils.decode('&lt;b&gt;total.js&lt;/b&gt;&amp;nbsp;') === '<b>total.js</b>&nbsp;', 'utils.decode()');
 
-	var result = utils.parseXML('<div><b>Peter</b><i style="color:red">Italic</i></div>');
+	var result = utils.parseXML('<div><b>Peter&amp;Janko</b><i style="color:red">Ita&apos;lic</i></div>');
 
-	assert.ok(result['div.b'] === 'Peter', 'XML Parser 1');
-	assert.ok(result['div.i'] === 'Italic', 'XML Parser 2');
+	assert.ok(result['div.b'] === 'Peter&Janko', 'XML Parser 1');
+	assert.ok(result['div.i'] === 'Ita\'lic', 'XML Parser 2');
 	assert.ok(result['div.i[]'].style === 'color:red', 'XML Parser Attributes');
 
 	result = utils.parseXML('<xml>OK</xml>');
