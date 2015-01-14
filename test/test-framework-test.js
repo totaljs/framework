d0073dc509beb5bc938c2cef1277c8ff485ab806
Peter Širka <petersirka@gmail.com>
2015-01-14 13:34:26 +0100

    new changes

--- test/test-framework-test.js
+++ test/test-framework-test.js
@@ -1,12 +1,14 @@
 var framework = require('../index');
 var url = 'http://127.0.0.1:8001/';
-var mem = require('memwatch');
 
 framework.onAuthorization = function(req, res, flags, cb) {
     req.user = { alias: 'Peter Širka' };
     req.session = { ready: true };
     cb(req.url === '/a/');
 };
+
+/*
+var mem = require('memwatch');
 
 mem.on('leak', function(info) {
     console.log('LEAK ->', info);
@@ -15,5 +17,6 @@ mem.on('leak', function(info) {
 mem.on('stats', function(info) {
     console.log('STATS ->', JSON.stringify(info));
 });
+*/
 
 framework.http('debug', { port: 8001 });
