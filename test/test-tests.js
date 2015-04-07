407506352844e8a582b828d60654f0b7e0d846fc
Peter Širka <petersirka@gmail.com>
2015-04-07 14:52:31 +0200

    updated version

--- test/test-tests.js
+++ test/test-tests.js
@@ -41,3 +41,55 @@ xi.note = 'Jano';
 qi.arr.push(xi);
 */
 //qi.$validate();
+
+Array.prototype.compare = function(id, arr, keys, callback) {
+
+    var self = this;
+    var cache = {};
+
+    for (var i = 0, length = arr.length; i < length; i++) {
+        var key = arr[i][id];
+        if (key === undefined)
+            continue;
+        cache[key] = i;
+    }
+
+    var kl = keys.length;
+
+    for (var i = 0, length = self.length; i < length; i++) {
+        var a = self[i];
+        var key = a[id];
+
+        if (!key)
+            continue;
+
+        var index = cache[key];
+
+        if (index === undefined) {
+            callback(0, item);
+            continue;
+        }
+
+        var b = arr[index];
+
+        for (var j = 0; j < kl; j++) {
+
+            var k = keys[j];
+            var av = a[k];
+            var bv = b[k];
+
+            if (av === bv)
+                continue;
+
+
+        }
+
+    }
+};
+
+var arr1 = [{ id: 1, name: 'Peter', age: 25 }, { id: 2, name: 'Lucia', age: 19 }, { id: 3, name: 'Jozef', age: 33 }];
+var arr2 = [{ id: 2, name: 'Lucka', age: 5 }, { id: 3, name: 'Peter', age: 50 }, { id: 1, name: 'Peter', age: 20 }, { id: 5, name: 'New', age: 33 }];
+
+arr1.compare('id', arr2, ['name', 'age'], function(type, item) {
+    console.log(type, item);
+});
