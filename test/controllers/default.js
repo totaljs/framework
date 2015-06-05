2cc8bdaeb129a8677c7a77b860b8a2a0f8c9f675
Peter Širka <petersirka@gmail.com>
2015-06-05 14:17:57 +0200

    fixed: test

--- test/controllers/default.js
+++ test/controllers/default.js
@@ -588,7 +588,7 @@ function viewViews() {
     assert.ok(output.contains('#ACAXXX#'), name + 'if');
     assert.ok(output.contains('<label><input type="radio" name="a" checked="checked" value="A" /> <span>test label</span></label>'), name + 'radio');
     assert.ok(output.contains('<div>NESTED</div>'), name + 'if - nested');
-    assert.ok(output.contains('---<div>Hello World!</div>---'), name + '- "/" view path problem');
+    assert.ok(output.contains('---<div>Hello World!</div><div>Price: 12</div>---'), name + '- "/" view path problem');
 
     self.json({
         r: true
