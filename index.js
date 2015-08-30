405ef6330d59334ba8b6ba15b1f10579d76ee1c5
Peter Širka <petersirka@gmail.com>
2015-08-31 00:11:21 +0200

    New version.

--- index.js
+++ index.js
@@ -227,7 +227,7 @@ function Framework() {
 
 	this.id = null;
 	this.version = 1910;
-	this.version_header = '1.9.1-17';
+	this.version_header = '1.9.1';
 
 	var version = process.version.toString().replace('v', '').replace(/\./g, '');
 	if (version[1] === '0')
