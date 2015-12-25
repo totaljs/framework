66860b0276ed00e1aed7dd5f7b9337c77f96cc4b
Peter Širka <petersirka@gmail.com>
2015-12-26 00:34:23 +0100

    Updated beta version.

--- index.js
+++ index.js
@@ -407,7 +407,7 @@ function Framework() {
 
 	this.id = null;
 	this.version = 1940;
-	this.version_header = '1.9.4-26';
+	this.version_header = '1.9.4-27';
 
 	var version = process.version.toString().replace('v', '').replace(/\./g, '');
 	if (version[0] !== '0' || version[1] !== '0')
