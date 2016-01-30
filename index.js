2dfea16cc04eb99cb38b8eca277129e5f303ed11
Peter Širka <petersirka@gmail.com>
2016-01-30 19:32:36 +0100

    Fixed view caching.

--- index.js
+++ index.js
@@ -428,7 +428,7 @@ function Framework() {
 
 	this.id = null;
 	this.version = 1970;
-	this.version_header = '1.9.7-11';
+	this.version_header = '1.9.7-12';
 
 	var version = process.version.toString().replace('v', '').replace(/\./g, '');
 	if (version[0] !== '0' || version[1] !== '0')
@@ -12717,7 +12717,7 @@ Controller.prototype.view = function(name, model, headers, isPartial) {
 	// views root `~~some_view`
 	// package    `@some_view`
 
-	var key = 'view#=' + this.themeName + '/' + name;
+	var key = 'view#=' + this.themeName + '/' + self._currentView + '/' + name;
 	var filename = framework.temporary.other[key];
 	var isLayout = self.isLayout;
 
