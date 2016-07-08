fab2bceaf9c70ab5eebca99d712f2a223008fa87
Peter Širka <petersirka@gmail.com>
2016-07-08 10:02:24 +0200

    Updated version.

--- builders.js
+++ builders.js
@@ -21,7 +21,7 @@
 
 /**
  * @module FrameworkBuilders
- * @version 2.0.0
+ * @version 2.0.1
  */
 
 'use strict';
--- index.js
+++ index.js
@@ -21,7 +21,7 @@
 
 /**
  * @module Framework
- * @version 2.0.0
+ * @version 2.0.1
  */
 
 'use strict';
