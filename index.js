bfff0bb18e0f4051ee51334be43cfa820bb70ada
Peter Širka <petersirka@gmail.com>
2016-08-31 09:54:05 +0200

    New beta version.

--- index.js
+++ index.js
@@ -468,7 +468,7 @@ function Framework() {
 
 	this.id = null;
 	this.version = 2020;
-	this.version_header = '2.0.2-5';
+	this.version_header = '2.0.2-6';
 	this.version_node = process.version.toString().replace('v', '').replace(/\./g, '').parseFloat();
 
 	this.config = {
