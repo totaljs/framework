c81eb1f783ce85f54e27402d8e316d45fb8ed4d2
Peter Širka <petersirka@gmail.com>
2016-12-12 20:25:48 +0100

    Update beta verson `v2.4.0-4`.

--- index.js
+++ index.js
@@ -488,7 +488,7 @@ function Framework() {
 
 	this.id = null;
 	this.version = 2400;
-	this.version_header = '2.4.0-3';
+	this.version_header = '2.4.0-4';
 	this.version_node = process.version.toString().replace('v', '').replace(/\./g, '').parseFloat();
 
 	this.config = {
