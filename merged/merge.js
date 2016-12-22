49197849144880fc93b14acdb6754b6f7e97b554
Peter Širka <petersirka@gmail.com>
2016-12-22 23:35:12 +0100

    Fix singlefile library.

--- merged/merge.js
+++ merged/merge.js
@@ -24,12 +24,14 @@ for (var i = 0, length = merge.length; i < length; i++) {
     var file = merge[i];
     var content = fs.readFileSync('../' + file).toString('utf8');
 
+    content = content.replace("!global.framework && require('./index');", '');
+
     switch (file) {
         case 'index.js':
             buffer.push(content);
             break;
         default:
-            buffer.push('(function(module){var exports=module.exports;global.framework_' + file.substring(0, file.length - 3) + '=module.exports;\n' + content + 'return module;})({ exports: {} });');
+            buffer.push('(function(module){var exports=module.exports;global.framework_' + file.substring(0, file.length - 3) + '=module.exports;\n' + content + 'return module;})({exports:{}});');
             break;
     }
 }
