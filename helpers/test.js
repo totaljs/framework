b6bfd82182d1bd652ccee41567ca87e2687b7419
Peter Širka <petersirka@gmail.com>
2017-07-04 08:27:40 +0200

    Add new helper file.

---
+++ helpers/test.js
@@ -0,0 +1,14 @@
+// ===================================================
+// FOR UNIT-TESTING
+// Total.js - framework for Node.js platform
+// https://www.totaljs.com
+// ===================================================
+
+const options = {};
+
+// options.ip = '127.0.0.1';
+// options.port = parseInt(process.argv[2]);
+// options.config = { name: 'Total.js' };
+// options.sleep = 3000;
+
+require('total.js').http('test', options);
