f1428ed755b342590c68bf18488a92e81bfddfb5
Peter Širka <petersirka@gmail.com>
2017-08-30 15:13:55 +0200

    Fix test.

--- test/test-css.js
+++ test/test-css.js
@@ -1,6 +1,4 @@
 var assert = require('assert');
-var utils = require('../utils');
-var utils = require('../index');
 var internal = require('../internal');
 
 var buffer = [];
@@ -13,7 +11,9 @@ buffer.push('@keyframes test{border-radius:5px}');
 buffer.push('div{background:linear-gradient(90deg, #000000, #FFFFFF);}');
 
 var css = buffer.join('\n');
-assert.ok(internal.compile_css(css) === 'b{border-radius:1px}a{border-radius:1px 2px 3px 4px}a{text-overflow:ellipsis}span{opacity:0;filter:alpha(opacity=0)}@keyframes test{border-radius:5px}@-webkit-keyframes test{border-radius:5px}@-moz-keyframes test{border-radius:5px}@-o-keyframes test{border-radius:5px}div{background:-webkit-linear-gradient(90deg,#000000,#FFFFFF);background:-moz-linear-gradient(90deg,#000000,#FFFFFF);background:-o-linear-gradient(90deg,#000000,#FFFFFF);background:-ms-linear-gradient(90deg,#000000,#FFFFFF);background:linear-gradient(90deg,#000000,#FFFFFF)}', 'automated CSS vendor prefixes');
+assert.ok(internal.compile_css(css) === 'b{border-radius:1px}a{border-radius:1px 2px 3px 4px}a{text-overflow:ellipsis}span{opacity:0;filter:alpha(opacity=0)}@keyframes test{border-radius:5px}@-webkit-keyframes test{border-radius:5px}@-moz-keyframes test{border-radius:5px}@-o-keyframes test{border-radius:5px}div{background:-webkit-linear-gradient(90deg,#000000,#FFFFFF);background:-moz-linear-gradient(90deg,#000000,#FFFFFF);background:-ms-linear-gradient(90deg,#000000,#FFFFFF);background:linear-gradient(90deg,#000000,#FFFFFF)}', 'automated CSS vendor prefixes');
+
+// console.log(internal.compile_css('/*auto*/\ndiv{background:repeating-linear-gradient(90deg, #000000, #FFFFFF);}'));
 
 css = '.input{ }, .input:disabled, .input:hover { background-color: red; } .required{content:"This, field is required"}';
 assert.ok(internal.compile_css(css) === '.input{},.input:disabled,.input:hover{background-color:red}.required{content:"This, field is required"}', 'Problem with content.');
