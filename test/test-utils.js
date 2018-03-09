7e52f6b3dedeef64bf9c2c96ead4e47f4d184a26
Peter Širka <petersirka@gmail.com>
2018-03-09 09:00:40 +0100

    Added missing time zone.

--- test/test-utils.js
+++ test/test-utils.js
@@ -4,11 +4,13 @@ global.builders = require('../builders');
 var assert = require('assert');
 var utils = require('../utils');
 
+process.env.TZ = 'utc';
+
 // test: date prototype
 function prototypeDate() {
 
 	var dt = new Date(1404723152167);
-	assert.ok(dt.toString() === 'Mon Jul 07 2014 10:52:32 GMT+0200 (CEST)', 'date problem');
+	assert.ok(dt.toString() === 'Mon Jul 07 2014 09:52:32 GMT+0100 (UTC)', 'date problem');
 	assert.ok(dt.format() === '2014-07-07T08:52:32.167Z', 'date format(0) problem');
 	assert.ok(dt.add('minute', 5).toString() === 'Mon Jul 07 2014 10:57:32 GMT+0200 (CEST)', 'date add');
 	assert.ok(dt.format('MMM') === 'Jul', 'month name 1');
