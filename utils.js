218bcb7d4d9a7cd0708864213effd972e26faa66
Peter Širka <petersirka@gmail.com>
2019-03-10 09:50:19 +0100

    Renamed `U.parseUA()` to `String.UA()`.

--- utils.js
+++ utils.js
@@ -3227,6 +3227,103 @@ SP.parseQuery = function() {
 	return exports.parseQuery(this);
 };
 
+SP.parseUA = function(ua) {
+
+	if (!ua)
+		return '';
+
+	var arr = ua.match(regexpUA);
+	var uid = '';
+	if (arr) {
+		var data = {};
+		for (var i = 0; i < arr.length; i++) {
+
+			if (arr[i] === 'like' && arr[i + 1] === 'Gecko') {
+				i += 1;
+				continue;
+			}
+
+			var key = arr[i].toLowerCase();
+			if (key === 'like')
+				break;
+
+			switch (key) {
+				case 'linux':
+				case 'windows':
+				case 'mac':
+				case 'iemobile':
+				case 'android':
+				case 'symbian':
+				case 'symbos':
+				case 'media':
+				case 'center':
+				case 'mobile':
+				case 'electron':
+				case 'tv':
+				case 'smarttv':
+				case 'smart':
+				case 'tizen':
+					data[arr[i]] = 1;
+					break;
+				case 'ipad':
+				case 'iphone':
+					data.iOS = 1;
+					data[arr[i]] = 1;
+					break;
+				case 'phone':
+					data.Mobile = 1;
+					data.Phone = 1;
+					break;
+				case 'mobi':
+					data.Mobile = 1;
+					break;
+				case 'blackberry':
+				case 'tizenbrowser':
+				case 'samsungbrowser':
+				case 'chrome':
+				case 'firefox':
+				case 'mini':
+				case 'msie':
+				case 'opera':
+				case 'outlook':
+				case 'safari':
+					data[arr[i]] = 1;
+					break;
+				case 'trident':
+					data.MSIE = 1;
+					break;
+				case 'opr':
+					data.Opera = 1;
+					break;
+			}
+		}
+
+		if (data.IEMobile) {
+			if (data.Android)
+				delete data.Android;
+			if (data.Safari)
+				delete data.Safari;
+			if (data.Chrome)
+				delete data.Chrome;
+		} else if (data.MSIE) {
+			if (data.Chrome)
+				delete data.Chrome;
+			if (data.Safari)
+				delete data.Safari;
+		} else if (data.Chrome) {
+			if (data.Safari)
+				delete data.Safari;
+		} else if (data.SamsungBrowser) {
+			if (data.Safari)
+				delete data.Safari;
+		}
+
+		uid = Object.keys(data).join(' ');
+	}
+
+	return uid;
+};
+
 SP.parseCSV = function(delimiter) {
 
 	if (!delimiter)
@@ -4091,7 +4188,7 @@ SP.decrypt = function(key, secret) {
 
 exports.encryptVAL = function(req, val, key, strict) {
 	var obj = {};
-	obj.ua = exports.parseUA(req.headers['user-agent']);
+	obj.ua = (req.headers['user-agent'] || '').parseUA();
 	if (strict)
 		obj.ip = req.ip;
 	obj.data = val;
@@ -4100,106 +4197,9 @@ exports.encryptVAL = function(req, val, key, strict) {
 
 exports.decryptVAL = function(req, val, key) {
 	var obj = F.decrypt(val, key || '', true);
-	if (!obj || (obj.ip && obj.ip !== req.ip) || (obj.ua !== exports.parseUA(req.headers['user-agent'])))
+	if (!obj || (obj.ip && obj.ip !== req.ip) || (obj.ua !== (req.headers['user-agent'] || '').parseUA()))
 		return;
 	return obj.data;
-};
-
-exports.parseUA = function(ua) {
-
-	if (!ua)
-		return '';
-
-	var arr = ua.match(regexpUA);
-	var uid = '';
-	if (arr) {
-		var data = {};
-		for (var i = 0; i < arr.length; i++) {
-
-			if (arr[i] === 'like' && arr[i + 1] === 'Gecko') {
-				i += 1;
-				continue;
-			}
-
-			var key = arr[i].toLowerCase();
-			if (key === 'like')
-				break;
-
-			switch (key) {
-				case 'linux':
-				case 'windows':
-				case 'mac':
-				case 'iemobile':
-				case 'android':
-				case 'symbian':
-				case 'symbos':
-				case 'media':
-				case 'center':
-				case 'mobile':
-				case 'electron':
-				case 'tv':
-				case 'smarttv':
-				case 'smart':
-				case 'tizen':
-					data[arr[i]] = 1;
-					break;
-				case 'ipad':
-				case 'iphone':
-					data.iOS = 1;
-					data[arr[i]] = 1;
-					break;
-				case 'phone':
-					data.Mobile = 1;
-					data.Phone = 1;
-					break;
-				case 'mobi':
-					data.Mobile = 1;
-					break;
-				case 'blackberry':
-				case 'tizenbrowser':
-				case 'samsungbrowser':
-				case 'chrome':
-				case 'firefox':
-				case 'mini':
-				case 'msie':
-				case 'opera':
-				case 'outlook':
-				case 'safari':
-					data[arr[i]] = 1;
-					break;
-				case 'trident':
-					data.MSIE = 1;
-					break;
-				case 'opr':
-					data.Opera = 1;
-					break;
-			}
-		}
-
-		if (data.IEMobile) {
-			if (data.Android)
-				delete data.Android;
-			if (data.Safari)
-				delete data.Safari;
-			if (data.Chrome)
-				delete data.Chrome;
-		} else if (data.MSIE) {
-			if (data.Chrome)
-				delete data.Chrome;
-			if (data.Safari)
-				delete data.Safari;
-		} else if (data.Chrome) {
-			if (data.Safari)
-				delete data.Safari;
-		} else if (data.SamsungBrowser) {
-			if (data.Safari)
-				delete data.Safari;
-		}
-
-		uid = Object.keys(data).join(' ');
-	}
-
-	return uid;
 };
 
 exports.encryptUID = function(val, key) {
