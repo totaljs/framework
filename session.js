fa2d7b8637ce26f24c7056bfaabdb266d44bf905
Peter Širka <petersirka@gmail.com>
2019-03-21 22:44:45 +0100

    Added `session` management.

--- session.js
+++ session.js
@@ -1,160 +1,222 @@
-const Fs = require('fs');
+require('./index');
 
-function Session() {
+const COOKIEOPTIONS = { httponly: true, security: 'lax' };
+const sessions = {};
+const Fs = require('fs');
+const filename = 'sessions{0}.txt';
+
+function Session(name) {
 	var t = this;
-	t.items = null;
-	t.load();
-	t.saveforce = function() {
-		Fs.writeFile(PATH.databases('sessions.json'), JSON.stringify(t.items), ERROR());
+	var timeoutsave = null;
+
+	t.name = name || '';
+	t.items = new Map();
+	t.$savecallback = ERROR('session.save');
+
+	// t.onremove = function(item)
+	// t.ondata = function(item, next(err, data))
+
+	t.$save = function() {
+		timeoutsave && clearTimeout(timeoutsave);
+		timeoutsave = setTimeout(t.$saveforce, 1000 * 10); // 10 seconds
+	};
+
+	t.$saveforce = function() {
+		var storage = [];
+		for (var m of t.items.values()) {
+			if (m.expire > NOW)
+				storage.push(m.uid + ';' + (m.id || '') + ';' + m.expire.getTime());
+			else {
+				self.onremove && self.onremove(m);
+				t.items.delete(m.uid);
+			}
+		}
+		Fs.writeFile(PATH.databases(filename.format((t.name ? ('_' + t.name) : ''))), storage.join('\n'), t.$savecallback);
+		timeoutsave = null;
 	};
 }
 
-Session.prototype.getcookie = function(opt) {
-
-	// opt.req
-	// opt.key
-	// opt.strict
-	// opt.value
-
+Session.prototype.find = function(filter, callback) {
 	var self = this;
-	var cookie = opt.req.cookie(opt.cookie);
+	var keys = Object.keys(filter);
+	for (var m of self.items.values()) {
+		if (m && m.data && m.expire >= NOW) {
+			for (var j = 0; j < keys.length; j++) {
+				var key = keys[j];
+				if (m.data[key] === filter[key])
+					callback(m);
+				else
+					break;
+			}
+		} else {
+			self.onremove && self.onremove(m);
+			self.items.delete(m.uid);
+			self.$save();
+		}
+	}
+};
 
-	if (!cookie || cookie.length < 15) {
-		opt.callback(null, null);
+Session.prototype.has = function(uid, callback) {
+	callback(null, this.items.has(uid));
+};
+
+Session.prototype.getcookie = function(req, opt, callback) {
+
+	// opt.name {String} A cookie name
+	// opt.expire {String} Expiration
+	// opt.key {String} Encrypt key
+	// opt.extendcookie {Boolean} Extends cookie expiration (default: true)
+
+	if (req.req)
+		req = req.req;
+
+	var token = req.cookie(opt.name);
+	if (!token || token.length < 20) {
+		callback();
 		return;
 	}
 
-	var value = DECRYPTREQ(opt.req, cookie, opt.key);
+	var value = DECRYPTREQ(req, token, opt.key);
 	if (value) {
-		opt.id = value.id;
-		opt.sessionid = value.sessionid;
-		self.get(opt);
+		value = value.split(';');
+		if (opt.expire && opt.extendcookie !== false)
+			req.res.cookie(opt.name, token, opt.expire, COOKIEOPTIONS);
+		this.get(value[0], opt.expire, callback);
 	} else
-		opt.callback(null, null);
+		callback();
 };
 
-Session.prototype.setcookie = function(opt) {
+Session.prototype.setcookie = function(res, opt, callback) {
 
-	// opt.req
-	// opt.key
-	// opt.strict
+	// opt.name {String} A cookie name
+	// opt.uid {String} A unique session ID
+	// opt.id {String} Optional, custom ID
+	// opt.expire {String} Expiration
+	// opt.strict {Boolean} Strict comparing of cookie according to IP (default: false)
+	// opt.key {String} Encrypt key
+	// opt.data {Object} A session data
+
+	if (res.res)
+		res = res.res;
+
+	this.set(opt.uid, opt.id, opt.data, opt.expire, function(err, item, meta) {
+		if (err) {
+			callback && callback(err);
+		} else {
+			var data = opt.uid + ';' + (opt.id || '');
+			var token = ENCRYPTREQ(res.req, data, opt.key, opt.strict);
+			res.cookie(opt.name, token, opt.expire, COOKIEOPTIONS);
+			callback && callback(null, item, meta);
+		}
+	});
+};
+
+Session.prototype.set = function(uid, id, data, expire, callback) {
+
+	if (typeof(id) === 'object') {
+		callback = expire;
+		expire = data;
+		data = id;
+		id = '';
+	}
 
 	var self = this;
 	var obj = {};
-
-	if (opt.id)
-		obj.id = opt.id;
-
-	if (opt.sessionid)
-		obj.sessionid = opt.sessionid;
-
-	var callback = opt.callback;
-
-	opt.callback = function(err) {
-		if (err)
-			callback(err);
-		else
-			callback(null, ENCRYPTREQ(opt.req, obj, opt.key || '', opt.strict));
-	};
-
-	self.set(opt);
+	obj.uid = uid;
+	obj.id = id == null ? '' : (id + '');
+	obj.expire = NOW.add(expire);
+	obj.data = data;
+	self.items.set(uid, obj);
+	callback && callback(null, data, obj);
+	self.$save();
 };
 
-Session.prototype.rem = function(opt) {
+Session.prototype.get = function(uid, expire, callback) {
 
-	var self = this;
-	var count = 0;
-
-	if (opt.id && self.items[opt.id]) {
-		count++;
-		delete self.items[opt.id];
+	if (typeof(expire) === 'function') {
+		callback = expire;
+		expire = null;
 	}
 
-	if (opt.sessionid && self.items[opt.sessionid]) {
-		count++;
-		delete self.items[opt.sessionid];
-	}
-
-	opt.callback && opt.callback(null, count);
-};
-
-Session.prototype.set = function(opt) {
-
-	// opt.id
-	// opt.sessionid
-	// opt.value
-
 	var self = this;
-	var obj = {};
-
-	if (opt.id)
-		obj.id = opt.id;
-
-	if (opt.sessionid)
-		obj.sessionid = opt.sessionid;
-
-	obj.expire = typeof(obj.expire) === 'string' ? NOW.add(obj.expire) : obj.expire instanceof Date ? obj.expire.getTime() : 0;
-	obj.value = opt.value;
-
-	if (opt.id)
-		self.items[opt.id] = obj;
-
-	if (opt.sessionid)
-		self.items[opt.sessionid] = obj;
-
-	opt.callback && opt.callback(null, obj);
-};
-
-Session.prototype.get = function(opt) {
-
-	// opt.id
-	// opt.sessionid
-	// opt.value
-
-	var self = this;
-	var meta = self.items[opt.id || opt.sessionid];
-
-	if (opt.callback) {
-
-		if (meta) {
-			opt.callback(null, meta.value, meta);
-			if (opt.expire) {
-				meta.expire = opt.expire;
-				self.set(meta);
-			}
-		} else
-			opt.callback(null, null);
-	}
-};
-
-Session.prototype.clear = function(callback) {
-
-	var self = this;
-	var keys = Object.keys(self.items);
-
-	for (var i = 0; i < keys.length; i++) {
-		var key = keys[i];
-		var item = self.items[key];
-		if (item == null || item.expire < NOW) {
-			delete self.items[key];
+	var item = self.items.get(uid);
+	if (item) {
+		if (item.expire < NOW) {
+			self.onremove && self.onremove(item);
+			self.items.delete(uid);
+			item = null;
+			self.$save();
+		} else if (expire) {
+			item.expire = NOW.add(expire);
+			self.items.set(uid, item);
 		}
 	}
 
+	// we need to load data
+	if (item && item.data == null) {
+		if (self.ondata) {
+			self.ondata(item, function(err, data) {
+				item.data = data;
+				callback(err, data, item);
+			});
+			return;
+		}
+	}
+
+	callback(null, item ? item.data : null, item);
+};
+
+Session.prototype.count = function(callback) {
+	callback(null, self.items.length);
+};
+
+Session.prototype.rem = function(uid, callback) {
+	var self = this;
+	self.$save();
+	var item = self.items.get(uid);
+	item && self.items.delete(uid);
+	callback && callback(null, item);
+	self.onremove && self.onremove(item);
+};
+
+Session.prototype.clear = function(callback) {
+	var self = this;
+	self.items.clear();
+	callback && callback();
+	self.$save();
+};
+
+Session.prototype.load = function(callback) {
+
+	var self = this;
+	var removed = 0;
+	var data = [];
+
+	try {
+		data = Fs.readFileSync(PATH.databases(filename.format((self.name ? ('_' + self.name) : '')))).toString('utf8').split('\n');
+	} catch (e) {}
+
+	for (var i = 0; i < data.length; i++) {
+		var item = data[i].split(';');
+		var obj = {};
+		obj.uid = item[0];
+		obj.id = item[1];
+		obj.expire = new Date(+item[2]);
+		obj.data = null;
+		if (obj.expire > NOW)
+			self.items.set(obj.uid, obj);
+		else
+			removed++;
+	}
+
+	removed && self.$save();
 	callback && callback();
 };
 
-Session.prototype.load = function() {
-	var self = this;
-	try {
-		var data = Fs.readFileSync(PATH.databases('sessions_' + self.name + '.json'));
-		self.items = data.toString('utf8').parseJSON(true) || {};
-	} catch (e) {}
+global.SESSION = function(name) {
+	if (sessions[name])
+		return sessions[name];
+	var session = new Session(name);
+	session.load();
+	return sessions[name] = session;
 };
-
-Session.prototype.save = function() {
-	var self = this;
-	clearTimeout(self.timeout);
-	self.timeout = setTimeout(self.saveforce, 20000);
-};
-
-global.SESSION = new Session();
