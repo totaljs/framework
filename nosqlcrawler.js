a20efbc10c0ba3f9550d28f03ba25b5eca7c3428
Peter Širka <petersirka@gmail.com>
2019-04-23 14:13:34 +0200

    Updated years

--- nosqlcrawler.js
+++ nosqlcrawler.js
@@ -1,4 +1,4 @@
-// Copyright 2018 (c) Peter Širka <petersirka@gmail.com>
+// Copyright 2018-2019 (c) Peter Širka <petersirka@gmail.com>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the
