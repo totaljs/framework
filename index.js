62120b3ad8b4363f325263c2a77de9e80eb64f8b
Peter Širka <petersirka@gmail.com>
2019-04-24 15:58:02 +0200

    Rollback.

--- index.js
+++ index.js
@@ -17582,7 +17582,7 @@ function controller_json_workflow(id) {
 
 			// IS IT AN OPERATION?
 			if (!self.route.schema.length) {
-				OPERATION(w.id, self.body, w.view ? self.callback(w.view) : self.callback(), self);
+				OPERATION(w.id, EMPTYOBJECT, w.view ? self.callback(w.view) : self.callback(), self);
 				return;
 			}
 
