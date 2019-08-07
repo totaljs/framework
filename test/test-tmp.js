255b8945a3dc9c447d903cf753a5645cb2e11806
Peter Širka <petersirka@gmail.com>
2019-08-07 12:07:22 +0200

    Fixed grammar.

--- test/test-tmp.js
+++ test/test-tmp.js
@@ -1,46 +1,14 @@
 require('../index');
 
-NEWTASK('cardav', function(push) {
-
-	push('getBooks', function($) {
-		console.log('----- getBooks');
-
-		$.value.books = [];
-		$.next('getContacts');
-	});
-
-	push('getContacts', function($) {
-
-		console.log('----- getContacts');
-
-		var current = $.value.books.shift();
-		if (current == null) {
-			// no books
-			$.success();
-			return;
-		}
-
-		// Download contacts
-		// ...
-		$.value.contacts = [];
-		$.next('synchronize');
-	});
-
-	push('synchronize', function($) {
-
-		console.log('----- synchronize');
-
-		// synchronize with DB
-		$.value.contacts;
-
-		// modify DB
-
-		// process next contacts
-		$.next('getContacts');
-	});
-
-});
-
-TASK('cardav', 'getBooks', function(err, response) {
-	console.log(err, response);
-});
+console.log(UIDF());
+console.log(UIDF());
+console.log(UIDF());
+console.log(UIDF());
+console.log(UIDF());
+console.log(UIDF());
+console.log(UIDF());
+console.log(UIDF());
+console.log(UIDF());
+console.log(UIDF());
+console.log(UIDF());
+console.log(UIDF());
