aec44631ee2167e3e5fce5d061c91563dca4efde
Peter Širka <petersirka@gmail.com>
2019-10-08 08:58:50 +0200

    Updated Flow.

--- flow.js
+++ flow.js
@@ -16,6 +16,7 @@ MP.clone = function() {
 	obj.data = self.data;
 	obj.prev = self.main.meta.flow[self.instanceid];
 	obj.count = self.count;
+	obj.processed = 0;
 	return obj;
 };
 
@@ -52,7 +53,7 @@ MP.send = function(output) {
 		var schema = meta.flow[output.id];
 		if (schema && schema.component) {
 			var next = meta.components[schema.component];
-			if (next && next.ondata) {
+			if (next && next.message) {
 				var inputindex = +output.index;
 				var message = self.clone();
 				message.count++;
@@ -62,10 +63,9 @@ MP.send = function(output) {
 				message.options = schema.options;
 				message.schema = schema;
 				message.duration2 = now;
-				message.processed = 0;
 				schema.stats.input++;
 				schema.stats.pending++;
-				next.ondata(message);
+				next.message(message);
 				count++;
 			}
 		}
@@ -101,43 +101,65 @@ MP.destroy = function() {
 	self.instance = null;
 };
 
-function Flow() {
+function Flow(name) {
 	var t = this;
+	t.name = name;
 	t.meta = {};
 	t.meta.components = {};
 	t.meta.flow = {};
 	t.meta.messages = 0;
+	t.$events = {};
+	new framework_utils.EventEmitter2(t);
 }
 
 var FP = Flow.prototype;
 
 FP.register = function(name, declaration) {
-
 	var self = this;
 
 	if (typeof(declaration) === 'string')
-		declaration = new Function('exports', declaration);
+		declaration = new Function('instance', declaration);
 
 	var prev = self.meta.components[name];
-	prev && prev.uninstall && prev.uninstall();
-	var curr = { id: name, main: self };
+	if (prev) {
+		prev.connected = false;
+		prev.disabled = true;
+		prev.disconnect && prev.disconnect();
+	}
+
+	var curr = { id: name, main: self, connected: true, disabled: false };
 	declaration(curr);
 	self.meta.components[name] = curr;
-	curr.install && curr.install();
+	self.$events.register && self.emit('register', name, curr);
+	curr.install && !prev && curr.install();
+	curr.connect && curr.connect();
 	return self;
 };
 
 FP.unregister = function(name) {
+
 	var self = this;
-	var prev = self.meta.components[name];
-	if (prev) {
-		prev.uninstall && prev.uninstall();
+
+	if (name == null) {
+		var keys = Object.keys(self.meta.components);
+		for (var i = 0; i < keys.length; i++)
+			self.unregister(self.meta.components[keys[i]]);
+		return self;
+	}
+
+	var curr = self.meta.components[name];
+	if (curr) {
+		self.$events.unregister && self.emit('unregister', name, curr);
+		curr.connected = false;
+		curr.disabled = true;
+		curr.disconnect && curr.disconnect();
+		curr.uninstall && curr.uninstall();
 		delete self.meta.components[name];
 	}
 	return self;
 };
 
-FP.use = function(schema) {
+FP.use = function(schema, callback) {
 	var self = this;
 
 	if (typeof(schema) === 'string')
@@ -147,15 +169,24 @@ FP.use = function(schema) {
 	// schema.COMPONENT_ID.options = {};
 	// schema.COMPONENT_ID.connections = { '0': [{ id: 'COMPONENT_ID', index: '2' }] }
 
+	var err = new ErrorBuilder();
 	var keys = Object.keys(schema);
-	for (var i = 0; i < keys.length; i++)
-		schema[keys[i]].stats = { pending: 0, input: 0, output: 0, duration: 0 };
+
+	for (var i = 0; i < keys.length; i++) {
+		var key = keys[i];
+		var instance = schema[key];
+		schema[key].stats = { pending: 0, input: 0, output: 0, duration: 0 };
+		var component = self.meta.components[instance.component];
+		if (!component)
+			err.push(key, '"' + instance.component + '" component not found.');
+	}
 
 	self.meta.flow = schema;
+	callback && callback(err.length ? err : null);
 	return self;
 };
 
-// path = COMPONENTID__INPUTINDEX
+// path = ID__INPUTINDEX
 FP.trigger = function(path, data) {
 	path = path.split('__');
 	var self = this;
@@ -163,7 +194,7 @@ FP.trigger = function(path, data) {
 	var schema = self.meta.flow[path[0]];
 	if (schema && schema.component) {
 		var instance = self.meta.components[schema.component];
-		if (instance && instance.ondata) {
+		if (instance && instance.message) {
 			var message = new Message();
 			message.duration = message.duration2 = Date.now();
 			message.count = 1;
@@ -180,12 +211,24 @@ FP.trigger = function(path, data) {
 			message.processed = 0;
 			schema.stats.input++;
 			schema.stats.pending++;
-			instance.ondata(message);
+			instance.message(message);
 			return true;
 		}
 	}
 };
 
-exports.make = function() {
-	return new Flow();
+FP.clear = function() {
+	var self = this;
+	self.meta.flow = {};
+	return self;
+};
+
+FP.make = function(fn) {
+	var self = this;
+	fn.call(self, self);
+	return self;
+};
+
+exports.make = function(name) {
+	return new Flow(name);
 };
