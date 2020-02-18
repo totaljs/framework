ee4c09502deda4f7fbd1f40f66674e75dfc12ae8
Peter Širka <petersirka@gmail.com>
2020-02-18 11:53:01 +0100

    Updated version,

--- index.js
+++ index.js
@@ -21,7 +21,7 @@
 
 /**
  * @module Framework
- * @version 3.4.0
+ * @version 3.4.1
  */
 
 'use strict';
@@ -914,8 +914,8 @@ function Framework() {
 	var self = this;
 
 	self.$id = null; // F.id ==> property
-	self.version = 3400;
-	self.version_header = '3.4.0';
+	self.version = 3410;
+	self.version_header = '3.4.1';
 	self.version_node = process.version.toString();
 	self.syshash = (__dirname + '-' + Os.hostname() + '-' + Os.platform() + '-' + Os.arch() + '-' + Os.release() + '-' + Os.tmpdir() + JSON.stringify(process.versions)).md5();
 	self.pref = global.PREF;
