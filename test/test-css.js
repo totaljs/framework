e8f41b415a1ec4a016d3a776c151dd3d3d525727
Peter Sirka <petersirka@gmail.com>
2020-04-22 00:18:35 +0200

    Fixed test.

--- test/test-css.js
+++ test/test-css.js
@@ -11,7 +11,7 @@ buffer.push('@keyframes test{border-radius:5px}');
 buffer.push('div{background:linear-gradient(90deg, #000000, #FFFFFF);}');
 
 var css = buffer.join('\n');
-assert.ok(internal.compile_css(css) === 'b{border-radius:1px}a{border-radius:1px 2px 3px 4px}a{text-overflow:ellipsis}span{opacity:0;filter:alpha(opacity=0)}@keyframes test{border-radius:5px}@-webkit-keyframes test{border-radius:5px}@-moz-keyframes test{border-radius:5px}@-o-keyframes test{border-radius:5px}div{background:-webkit-linear-gradient(90deg,#000000,#FFFFFF);background:-moz-linear-gradient(90deg,#000000,#FFFFFF);background:-ms-linear-gradient(90deg,#000000,#FFFFFF);background:linear-gradient(90deg,#000000,#FFFFFF)}', 'automated CSS vendor prefixes');
+assert.ok(internal.compile_css(css) === 'b{border-radius:1px}a{border-radius:1px 2px 3px 4px}a{text-overflow:ellipsis}span{opacity:0;filter:alpha(opacity=0)}@keyframes test{border-radius:5px}@-webkit-keyframes test{border-radius:5px}@-moz-keyframes test{border-radius:5px}@-o-keyframes test{border-radius:5px}div{background:-webkit-linear-gradient(90deg,#000,#FFF);background:-moz-linear-gradient(90deg,#000,#FFF);background:-ms-linear-gradient(90deg,#000,#FFF);background:linear-gradient(90deg,#000,#FFF)}', 'automated CSS vendor prefixes');
 
 css = '.input{ }, .input:disabled,	.input:hover { background-color: red; } .required{content:"This, field is required"}';
 assert.ok(internal.compile_css(css) === '.input{},.input:disabled,.input:hover{background-color:red}.required{content:"This, field is required"}', 'Problem with content.');
